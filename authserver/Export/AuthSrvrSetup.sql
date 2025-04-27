bdc086727d02584c34db8e8bc106bb14a625ed64
Sigrud <sigrud.ds@gmail.com>
2025-04-27 11:31:16 -0700

    Update AuthSrvrSetup.sql

--- authserver/Export/AuthSrvrSetup.sql
+++ authserver/Export/AuthSrvrSetup.sql
@@ -27,7 +27,7 @@ COLLATE=utf8mb4_general_ci
 COMMENT='';
 
 -- AuthSrvrTemplate.Test definition
-CREATE TABLE `Test` (
+CREATE TABLE IF NOT EXISTS `AuthSrvrTemplate`.`Test` (
   `Column1` tinyint(3) unsigned NOT NULL,
   `Column2` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
