312af112fc46282b5974a0d34fbe9540a8740c51
Shawn Shipton <sigrud.ds@gmail.com>
2025-04-27 18:04:08 -0700

    Typo Fix

--- authserver/Scripts/Classes/cDbTask.gd
+++ authserver/Scripts/Classes/cDbTask.gd
@@ -10,11 +10,13 @@ var stmt: String
 var success_func: Callable
 var fail_func: Callable
 
-func _init(p_type: Types, 
-	p_stmt: String, 
-	p_success_func := Callable(), 
-	p_fail_func := Callable()) -> void:
+
+func _init(
+		p_type: Types, 
+		p_stmt: String, 
+		p_success_func := Callable(), 
+		p_fail_func := Callable()) -> void:
 	type = p_type
 	stmt = p_stmt
 	success_func = p_success_func
-	fail_func = fail_func
+	fail_func = p_fail_func
