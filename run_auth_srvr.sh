3a5a12d2dfab6b0b6f5cb980f84f3cb9c75884ac
Sigrud <sigrud.ds@gmail.com>
2025-06-22 12:23:29 -0700

    created similar file pathing between VM's

--- run_auth_srvr.sh
+++ run_auth_srvr.sh
@@ -2,7 +2,7 @@
 
 #!/bin/bash
 
-GODOT_BIN=~/dev/GodotBuilds/Godot_v4.4.1-stable_linux.x86_64
+GODOT_BIN=~/dev/GodotEngine-Builds/Godot_v4.4.1-stable_linux.x86_64
 PROJECT_PATH=~/dev/GodotProj/godot-4-mariadb-multiplayer-tutorial-series/authserver
 
 "$GODOT_BIN" --headless --path "$PROJECT_PATH"
