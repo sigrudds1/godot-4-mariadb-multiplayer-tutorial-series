df45cb00c5a600fd482c8c8824ef64698cf8a5e5
Sigrud <sigrud.ds@gmail.com>
2025-08-03 13:27:07 -0700

    Bug fix on deleting msg blocked players

--- GameServer/Scenes/Player.gd
+++ GameServer/Scenes/Player.gd
@@ -18,7 +18,6 @@ var plyr_id: int
 var display_name: String = ""
 
 var inventory: Dictionary = {}
-var msg_blocked_plyrs: PackedStringArray = []
 
 var match_state: DataTypes.PlayerMatchStates = DataTypes.PlayerMatchStates.IDLE
 # TODO when player is awaiting or playing a match do not allow match setup changes
@@ -26,35 +25,30 @@ var match_state: DataTypes.PlayerMatchStates = DataTypes.PlayerMatchStates.IDLE
 var _connection_state: ConnectionStates =  ConnectionStates.ONLINE
 #var _db_inventory: Dictionary = Dictionary()
 var _db_msg_blocked_plyrs: PackedStringArray = []
+var _msg_blocked_plyrs: PackedStringArray = []
 
 
 func _ready() -> void:
-	if MessagingIface.sPlayerSentMsg.connect(_send_msg) != OK:
-		pass # TODO handle error
+	if MessagingIface.sPlayerSentMsg.connect(_send_msg) != OK: pass # TODO handle error
 	
-	if TimeLapse.sFiveMinutesLapsed.connect(_update_db) != OK:
-		pass # TODO handle error
+	if TimeLapse.sOneMinuteLapsed.connect(_update_db) != OK: pass # TODO handle error
 	
 	var thr: Thread = Thread.new()
 	var err_code: Error = thr.start(_update_db_thread_func.bind(thr, UpdateStates.CONNECT))
-	if err_code != OK:
-		printerr("player _update_thread start error code:" + str(err_code))
+	if err_code != OK: printerr("player _update_thread start error code:" + str(err_code))
 
 
 func add_msg_blocked_plyr(p_plyr_dname: String) -> void:
 	# NEVER Trust the client to send you something clean
-	if not Sanitize.check_display_name(p_plyr_dname):
-		return
+	if not Sanitize.check_display_name(p_plyr_dname): return
 	
-	if msg_blocked_plyrs.find(p_plyr_dname) == -1:
-		if not msg_blocked_plyrs.push_back(p_plyr_dname): 
-			pass
+	if _msg_blocked_plyrs.find(p_plyr_dname) == -1:
+		if not _msg_blocked_plyrs.push_back(p_plyr_dname): pass
 
 
 func remove_msg_blocked_plyr(p_plyr_dname: String) -> void:
-	var idx: int = msg_blocked_plyrs.find(p_plyr_dname)
-	if idx > 0:
-		msg_blocked_plyrs.remove_at(idx)
+	var idx: int = _msg_blocked_plyrs.find(p_plyr_dname)
+	if idx > -1: _msg_blocked_plyrs.remove_at(idx)
 
 
 # You may be asking why not queue_free from outside and _exit_tree instead
@@ -85,23 +79,23 @@ func _fetch_db_inventory(_thread: Thread) -> void:
 
 
 func _send_msg(p_from_plyr: String, p_msg: String) -> void:
-	if msg_blocked_plyrs.find(p_from_plyr) > -1: return
+	if _msg_blocked_plyrs.find(p_from_plyr) > -1: return
 	
 	MessagingIface.srvr_send_msg.rpc_id(peer_id, p_from_plyr, p_msg)
 
 
 func _update_blocked_plyr_list(p_thread: Thread) -> void:
-	msg_blocked_plyrs.clear()
+	_msg_blocked_plyrs.clear()
 	var blocked_plyr_rows: Array[Dictionary] = _fetch_db_blocked_plyrs(p_thread)
 	for row: Dictionary in blocked_plyr_rows:
 		var blocked: String = row.get("display_name")
-		if blocked != null and msg_blocked_plyrs.find(blocked) == -1:
+		if blocked != null and _msg_blocked_plyrs.find(blocked) == -1:
 			if _db_msg_blocked_plyrs.push_back(blocked): pass
 		else:
 			printerr("Player._get_msg_block null value or duplicate in rows:", blocked_plyr_rows)
 	
-	msg_blocked_plyrs = _db_msg_blocked_plyrs.duplicate() # must duplicate arrays are passed by ref
-	MessagingIface.srvr_send_blocked_players.rpc_id.call_deferred(peer_id, msg_blocked_plyrs)
+	_msg_blocked_plyrs = _db_msg_blocked_plyrs.duplicate() # must duplicate arrays are passed by ref
+	MessagingIface.srvr_send_blocked_players.rpc_id.call_deferred(peer_id, _msg_blocked_plyrs)
 
 
 func _update_db() -> void:
@@ -111,12 +105,13 @@ func _update_db() -> void:
 
 
 func _update_db_blocked_plyrs(p_thread: Thread) -> void:
+	print("_update_db_blocked_plyrs")
 	if p_thread == null: return
 	
 	var db_del_params_list: Array = []
 	
 	for plyr_dname:String in _db_msg_blocked_plyrs:
-		if msg_blocked_plyrs.find(plyr_dname) == -1:
+		if _msg_blocked_plyrs.find(plyr_dname) == -1:
 			var sql_params: Array[Dictionary] = [
 				{MariaDBConnector.FT_INT_U: plyr_id},
 				{MariaDBConnector.FT_VARCHAR: plyr_dname}
@@ -124,7 +119,7 @@ func _update_db_blocked_plyrs(p_thread: Thread) -> void:
 			db_del_params_list.push_back(sql_params)
 	
 	var db_insert_params_list: Array = []
-	for plyr_dname: String in msg_blocked_plyrs:
+	for plyr_dname: String in _msg_blocked_plyrs:
 		if _db_msg_blocked_plyrs.find(plyr_dname) == -1:
 			var sql_params: Array[Dictionary] = [
 				{MariaDBConnector.FT_INT_U: plyr_id},
@@ -143,9 +138,11 @@ func _update_db_blocked_plyrs(p_thread: Thread) -> void:
 		OS.delay_msec(DB.kThreadLoopDelay) 
 		db_conn = DB.get_db_conn()
 	
-	if db_conn == null: return
+	if db_conn == null:
+		printerr("Player._update_db_blocked_plyrs cannot get DB connection")
+		return
 	
-	for params:Array[Dictionary] in db_del_params_list:
+	for params: Array[Dictionary] in db_del_params_list:
 		task.params = params
 		db_conn.do_task_keep_issued(task)
 		if qr.error != MariaDBConnector.ErrorCode.OK:
@@ -153,7 +150,7 @@ func _update_db_blocked_plyrs(p_thread: Thread) -> void:
 	
 	task.stmt_glb_id = DB.StmtIDs.INSERT_MSG_BLOCKED_PLYR
 	
-	for params:Array[Dictionary] in db_insert_params_list:
+	for params: Array[Dictionary] in db_insert_params_list:
 		task.params = params
 		db_conn.do_task_keep_issued(task)
 		if qr.error != MariaDBConnector.ErrorCode.OK:
@@ -181,6 +178,7 @@ func _update_db_thread_func(p_this_thread: Thread, p_update_state: UpdateStates)
 
 
 func _update_plyr_status(p_thread: Thread) -> void:
+	print("_update_plyr_status")
 	if p_thread == null: return
 	
 	var stmt_id: DB.StmtIDs = DB.StmtIDs.INSERT_OR_UPDATE_PLYR
